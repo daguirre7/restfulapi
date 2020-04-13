37c1f77ea17fe7b4a0b70cf2b9a8e2d6597c8f4a
daguirre7 <daaguirre777@gmail.com>
2020-04-13 00:00:52 -0600

    Configuracion conexion .env oculto

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import {Maincontroller}from "./controllers/main.controller"
 
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
+
+
+import { config } from "dotenv";
+import { resolve } from "path";
+config({path:resolve(__dirname,"../.env")});
 
 class App{
     public app:Application;
@@ -11,11 +17,23 @@ class App{
     constructor(){
         this.app =express();
         this.setConfig();   
+        this.setMongoDBConfig();
         this.mainController= new Maincontroller(this.app);
     }
     private setConfig(){
         this.app.use(bodyParser.json({limit:"50mb"}));
         this.app.use(bodyParser.urlencoded({limit:"50mb",extended:true}))
+    }
+    //Configuracion propia de MongoDB
+    private setMongoDBConfig(){
+        mongoose.Promise = global.Promise;
+        mongoose.connect(process.env.MONGO_URI!,{useNewUrlParser:true, useUnifiedTopology:true},(err:any)=>{
+            if(err){
+                console.log(err.messsage);
+            }else{
+                console.log("Conexion exitosa");
+            }        
+        });
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import app from "./app";
+import {config} from "dotenv";
+import {resolve} from "path";
+
+config({path:resolve(__dirname,"../../.env")});
 
 app.listen(3000, ()=>{
     console.log("server web prendido en el puerto 3000");
